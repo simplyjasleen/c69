f02eaf727b8b0b1d9d1e1a4ff49b1ec4971c7f60
simplyjasleen <kjasleen.arora@gmail.com>
2021-02-14 08:14:07 +0530

    c69

--- screens/BookTransactionScreen.js
+++ screens/BookTransactionScreen.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
 
 export default class TransactionScreen extends React.Component {
     render() {
       return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
           <Text>Issue or Return</Text>
+          <TouchableOpacity>
+            <Text>Scan QR Code </Text>
+          </TouchableOpacity>
         </View>
       );
     }
